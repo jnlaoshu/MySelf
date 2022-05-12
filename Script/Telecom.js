4a8054dee7ea68bf58351ce27188833a38003f44
laoshu <79401373+jnlaoshu@users.noreply.github.com>
2022-05-12 12:19:26 +0800

    Update Telecom.js

--- Script/Telecom.js
+++ Script/Telecom.js
@@ -1,10 +1,3 @@
-/*
-中国电信流量信息
-𝐔𝐑𝐋：https://raw.githubusercontent.com/jnlaoshu/MySelf/main/Script/Telecom.js
-𝐅𝐫𝐨𝐦：https://github.com/mieqq/mieqq/blob/master/telecom.js
-𝐔𝐩𝐝𝐚𝐭𝐞：2022.05.12 09:00
-*/
-
 const url = "https://e.189.cn/store/user/package_detail.do";
 const url2 = "https://e.189.cn/store/user/balance_new.do";
 
