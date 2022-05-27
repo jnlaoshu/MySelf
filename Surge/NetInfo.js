a49240b3f6fcccf22bc44e82862c05f02f9a735f
laoshu <79401373+jnlaoshu@users.noreply.github.com>
2022-05-27 13:33:39 +0800

    Update NetInfo.js

--- Surge/NetInfo.js
+++ Surge/NetInfo.js
@@ -65,11 +65,11 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
       title: wifi.ssid ? wifi.ssid : cellularInfo,
       content:
         `[IP 地址]\n` +
-        (v4.primaryAddress ? `v4 @ ${v4.primaryAddress} \n` : '') +
-        (v6.primaryAddress ? `v6 @ ${v6.primaryAddress}\n` : '') +
+        (v4.primaryAddress ? `v4 ： ${v4.primaryAddress} \n` : '') +
+        (v6.primaryAddress ? `v6 ： ${v6.primaryAddress}\n` : '') +
         (v4.primaryRouter && wifi.ssid ? `Router v4 @ ${v4.primaryRouter}\n` : '') +
         (v6.primaryRouter && wifi.ssid ? `Router IPv6 @ ${v6.primaryRouter}\n` : '') +
-        `[节点 IP] ${info.query}\n` +
+        `[节点  IP] ${info.query}\n` +
         `[节点 ISP] ${info.isp}\n` +
         `[节点位置] ${getFlagEmoji(info.countryCode)} | ${info.country} - ${info.city
         }`,
