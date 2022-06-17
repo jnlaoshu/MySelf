322c97ca135a3ee96d0dfa5aa5700095049b3435
laoshu <79401373+jnlaoshu@users.noreply.github.com>
2022-06-17 13:41:19 +0800

    Update NetInfo.js

--- Surge/NetInfo.js
+++ Surge/NetInfo.js
@@ -5,10 +5,14 @@
  
 /*
 [Script]
-网络信息 = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/jnlaoshu/MySelf/master/Surge/NetInfo.js
+网络信息 = type=generic,timeout=30,script-path=https://raw.githubusercontent.com/jnlaoshu/MySelf/master/Surge/NetInfo.js
 	
 [Panel]
 网络信息 = script-name=网络信息,title=网络信息,content=请刷新,style=info,update-interval=1
+
+[Host]
+ip-api.com = 208.95.112.1
+
 */
 
 /**
