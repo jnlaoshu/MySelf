dc6b22020722bb88ca9ffe2a55c8170d43e00726
laoshu <79401373+jnlaoshu@users.noreply.github.com>
2022-06-20 15:35:13 +0800

    Update StartTime.js

--- Surge/StartTime.js
+++ Surge/StartTime.js
@@ -48,7 +48,7 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"SurgePRO | 2023-04-07",
+    title:"SurgePro | 2023-04-07",
     content: "运行时长："+startTime + "\n𝐌𝐢𝐭𝐌"+icon_status(mitm_status.enabled)+"   𝐑𝐞𝐰𝐫𝐢𝐭𝐞"+icon_status(rewrite_status.enabled)+"   𝐒𝐜𝐫𝐢𝐩𝐭𝐢𝐧𝐠"+icon_status(scripting_status.enabled),
     icon: icon_s?"power.circle":"exclamationmark.triangle",
    "icon-color":icon_s?"#FF2121":"#F20C00"
