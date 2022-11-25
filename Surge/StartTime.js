b2110be0576d865e6229d73c4b0165cb77043f7d
laoshu <79401373+jnlaoshu@users.noreply.github.com>
2022-11-25 17:27:57 +0800

    Update StartTime.js

--- Surge/StartTime.js
+++ Surge/StartTime.js
@@ -14,36 +14,26 @@
 运行时长 = script-name=运行时长,title=运行时长,content=请刷新,update-interval=1
 */
 
+let params = getParams($argument)
+
 !(async () => {
-let traffic = (await httpAPI("/v1/traffic","GET"));
-let dateNow = new Date();
-let dateTime = Math.floor(traffic.startTime*1000);
-let startTime = timeTransform(dateNow,dateTime);
-let mitm_status = (await httpAPI("/v1/features/mitm","GET"));
-let rewrite_status = (await httpAPI("/v1/features/rewrite","GET"));
-let scripting_status = (await httpAPI("/v1/features/scripting","GET"));
-let icon_s = mitm_status.enabled&&rewrite_status.enabled&&scripting_status.enabled;
-//点击按钮，刷新dns
-//if ($trigger == "button") await httpAPI("/v1/dns/flush");
-//点击按钮，重载配置（同时刷新dns）
-if ($trigger == "button") {
-	await httpAPI("/v1/profiles/reload");
-	$notification.post("配置重载","配置重载成功","")
-};
-$done({
-    title:"Surge  已运行"+startTime,
-    content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
-    icon: icon_s?"checkmark.seal":"exclamationmark.triangle",
-   "icon-color":icon_s?"#16A951":"#FF7500"
-});
+/* 时间获取 */
+let traffic = (await httpAPI("/v1/traffic","GET"))
+let dateNow = new Date()
+let dateTime = Math.floor(traffic.startTime*1000)
+let startTime = timeTransform(dateNow,dateTime)
+
+if ($trigger == "button") await httpAPI("/v1/dns/flush");
+
+  $done({
+      title:"𝗦𝗨𝗥𝗚𝗘 𝗣𝗥𝗢 ®",
+      content:`启动时长：${startTime}`,
+		icon: params.icon,
+		"icon-color":params.color
+    });
+
 })();
-function icon_status(status){
-  if (status){
-    return "\u2611";
-  } else {
-      return "\u2612"
-    }
-}
+
 function timeTransform(dateNow,dateTime) {
 let dateDiff = dateNow - dateTime;
 let days = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
@@ -57,19 +47,32 @@ let leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
 let seconds=Math.round(leave3/1000)
 
 if(days==0){
-  if(hours==0){
-    if(minutes==0)return(`${seconds}秒`);
-      return(`${minutes}分${seconds}秒`)
-    }
-    return(`${hours}时${minutes}分${seconds}秒`)
-  }else {
-        return(`${days}天${hours}时${minutes}分`)
+
+	if(hours==0){
+	if(minutes==0)return(`${seconds}秒`);
+	return(`${minutes}分${seconds}秒`)
 	}
+	return(`${hours}时${minutes}分${seconds}秒`)
+	}else {
+	return(`${days}天${hours}时${minutes}分`)
+	}
+
 }
+
+
 function httpAPI(path = "", method = "POST", body = null) {
-  return new Promise((resolve) => {
-    $httpAPI(method, path, body, (result) => {
-      resolve(result);
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
     });
-  });
+}
+
+function getParams(param) {
+  return Object.fromEntries(
+    $argument
+      .split("&")
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
 }
